ab8687bde0887d5bde5f6c6bdac718f7c284c4a7
IdealDev <idealdev1010@gmail.com>
2025-05-22 12:20:55 -0400

    added features PLP

--- assets/custom.js
+++ assets/custom.js
@@ -1,7 +1,7 @@
-  document.querySelectorAll('.card-product-selector').forEach(link => {
+  document.querySelectorAll('.bs-devshop__card-product-selector').forEach(link => {
     link.addEventListener('click', function(event) {
       event.preventDefault();
-      const card = link.closest('.card-wrapper').querySelector('.zoom-trigger');
+      const card = link.closest('.card-wrapper').querySelector('.bs-devshop__zoom-trigger');
       card.classList.add('zooming');
       setTimeout(() => {
         window.location.href = link.href;
